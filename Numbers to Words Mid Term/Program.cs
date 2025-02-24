81c811446bcb89821dd8c6003cddf854d0957e47
kirah-cox <kirahanncox@gmail.com>
2025-02-23 22:11:53 -0700

    Add project files.

---
+++ Numbers to Words Mid Term/Program.cs
@@ -0,0 +1,113 @@
+﻿using MyTools;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<char, string> singleNumberDictionary = new Dictionary<char, string>()
+        {
+            { '0', "" },
+            { '1', "one" },
+            { '2',  "two" },
+            { '3',  "three" },
+            { '4',  "four" },
+            { '5',  "five" },
+            { '6',  "six" },
+            { '7',  "seven" },
+            { '8',  "eight" },
+            { '9',  "nine" },
+
+        };
+
+        Dictionary<string, string> doubleNumberDictionary = new Dictionary<string, string>()
+        {
+            { "10",  "ten" },
+            { "11",  "eleven" },
+            { "12",  "twelve" },
+            { "13",  "thirteen" },
+            { "14",  "fourteen" },
+            { "15",  "fifteen" },
+            { "16",  "sixteen" },
+            { "17",  "seventeen" },
+            { "18",  "eighteen" },
+            { "19",  "nineteen" },
+            { "2",  "twenty" },
+            { "3",  "thirty" },
+            { "4",  "forty" },
+            { "5",  "fifty" },
+            { "6",  "sixty" },
+            { "7",  "seventy" },
+            { "8",  "eighty" },
+            { "9",  "ninety" },
+        };
+
+
+
+        Console.WriteLine("Hello, welcome to the bank! Select an option:");
+
+        while (!Tools.ExitSystem)
+        {
+            int optionResponse = 0;
+            bool correctResponse = false;
+            while (!correctResponse)
+            {
+                Console.WriteLine("1: View Account");
+                Console.WriteLine("2: Deposit into Account");
+                Console.WriteLine("3: Withdraw from Account");
+                Console.WriteLine("4: Exit");
+
+                if (!int.TryParse(Console.ReadLine().Trim(), out optionResponse))
+                {
+                    Console.WriteLine("Please select a valid number option.");
+                }
+                else
+                {
+                    correctResponse = true;
+                }
+            }
+            
+
+            bool passedCheck = false;
+
+            switch (optionResponse)
+            {
+                case 1:
+                    Tools.AccountToWords(singleNumberDictionary, doubleNumberDictionary);
+                    break;
+
+                case 2:
+                    Console.WriteLine("How much would you like to deposit?");
+                    passedCheck = Tools.CheckAndConvertResponse();
+                    if (!passedCheck)
+                    {
+                        break;
+                    }
+                    Tools.ResponseToWords(singleNumberDictionary, doubleNumberDictionary);
+                    Tools.Deposit(Tools.TrueAmountResponse);
+                    break;
+
+                case 3:
+                    Console.WriteLine("How much would you like to withdraw?");
+                    passedCheck = Tools.CheckAndConvertResponse();
+                    if (!passedCheck)
+                    {
+                        break;
+                    }
+                    Tools.ResponseToWords(singleNumberDictionary, doubleNumberDictionary);
+                    Tools.Withdraw(Tools.TrueAmountResponse);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thank you for using the bank! Exiting the system.");
+                    Tools.ExitSystem = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Please select a valid number option.");
+                    break;
+            }
+        
+        }
+    }
+}
---
+++ Numbers to Words Mid Term/Tools.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTools
+{
+    public class Tools
+    {
+        public static decimal AmountInAccount { get; set; } = 0.00m;
+        public static decimal TrueAmountResponse { get; set; } = 0;
+        public static string AmountResponse { get; set; } = "";
+        public static string NumbersAsWords { get; set; } = "";
+        public static bool OneDollar { get; set; } = false;
+        public static int LineNumber { get; set; } = 0;
+        public static bool ExitSystem { get; set; } = false;
+
+        public static bool CheckAndConvertResponse()
+        {
+            AmountResponse = Console.ReadLine();
+            char[] charAmountResponse = AmountResponse.ToCharArray();
+            string updatedAmountResponse = "";
+
+            if (AmountResponse == "" || AmountResponse == null)
+            {
+                Console.WriteLine("Please put the amount in the format of xxx,xxx.xx with no zeroes in front.");
+                return false;
+            }
+
+            if (charAmountResponse[0] == '-')
+            {
+                Console.WriteLine("You cannot deposit or withdraw a negative number.");
+                return false;
+            }
+
+            foreach (char c in charAmountResponse)
+            {
+                if (c != ',')
+                {
+                    updatedAmountResponse += c;
+                }
+            }
+
+            if (!decimal.TryParse(updatedAmountResponse, out decimal convertedAmountResponse))
+            {
+                Console.WriteLine("The amount needs to be in number form. Please put the amount in the format of xxx,xxx.xx with no zeroes in front.");
+                return false;
+            }
+
+            if (convertedAmountResponse < 1.00m)
+            {
+                Console.WriteLine("The minimum amount to deposit or withdraw is $1.00.");
+                return false;
+            }
+
+            string checkResponse = convertedAmountResponse.ToString("#,0.00");
+
+            if (checkResponse != AmountResponse)
+            {
+                Console.WriteLine("Please put the amount in the format of xxx,xxx.xx with no zeroes in front.");
+                return false;
+            }
+
+            TrueAmountResponse = convertedAmountResponse;
+            return true;
+        }
+
+
+        public static void Deposit(decimal amount)
+        {
+            if (AmountInAccount + amount > 999_999_999_999.99m)
+            {
+                Console.WriteLine("You cannot have more than $999,999,999,999.99 in your account.");
+                return;
+            }
+
+            AmountInAccount += amount;
+
+            Console.WriteLine($"You have deposited {NumbersAsWords}.");
+
+            TrueAmountResponse = 0;
+        }
+
+        public static void Withdraw(decimal amount)
+        {
+            if (AmountInAccount - amount < 0.00m)
+            {
+                Console.WriteLine("You do not have enough in your account to withdraw that amount.");
+                return;
+            }
+
+            AmountInAccount -= amount;
+
+            Console.WriteLine($"You have withdrawn {NumbersAsWords}.");
+
+            TrueAmountResponse = 0;
+        }
+
+        public static void AccountToWords(Dictionary<char, string> singleNumberDictionary, Dictionary<string, string> doubleNumberDictionary)
+        {
+            NumbersAsWords = "";
+            LineNumber = 0;
+            OneDollar = false;
+
+            int amountInAccountLength = AmountInAccount.ToString().Length;
+            string localAmountInAccount = AmountInAccount.ToString();
+            
+            string amountInAccount = AmountInAccount.ToString("#,0.00");
+            string[] amountInAccountArray = amountInAccount.Split(',');
+
+            foreach (string amount in amountInAccountArray)
+            {
+                MainConvert(amount, singleNumberDictionary, doubleNumberDictionary, amountInAccountLength, localAmountInAccount);
+
+                LineNumber++;
+            }
+
+            NumbersAsWords = NumbersAsWords.Trim();
+
+            Console.WriteLine($"You have {NumbersAsWords} in your account.");
+        }
+
+        public static void ResponseToWords(Dictionary<char, string> singleNumberDictionary, Dictionary<string, string> doubleNumberDictionary)
+        {
+            NumbersAsWords = "";
+            LineNumber = 0;
+            OneDollar = false;
+
+            int trueAmountAccountLength = TrueAmountResponse.ToString().Length;
+            string localTrueAmountResponse = TrueAmountResponse.ToString();
+
+            string[] amountInResponse = AmountResponse.Split(',');
+
+            foreach (string amount in amountInResponse)
+            {
+                MainConvert(amount, singleNumberDictionary, doubleNumberDictionary, trueAmountAccountLength, localTrueAmountResponse);
+
+                LineNumber++;
+            }
+
+            NumbersAsWords = NumbersAsWords.Trim();
+        }
+
+        public static void MainConvert(string amount, Dictionary<char, string> singleNumberDictionary, Dictionary<string, string> doubleNumberDictionary, int numberLength, string localTrueAndAccount)
+        {
+            switch (amount.Length)
+            {
+                case 1:
+                    ConvertWithOneNumber(amount, singleNumberDictionary, doubleNumberDictionary, numberLength, localTrueAndAccount);
+                    break;
+
+                case 2:
+                    ConvertWithTwoNumbers(amount, singleNumberDictionary, doubleNumberDictionary, numberLength, localTrueAndAccount);
+                    break;
+
+                case 3:
+                    ConvertWithThreeNumbers(amount, singleNumberDictionary, doubleNumberDictionary, numberLength, localTrueAndAccount);
+                    break;
+
+                case 4:
+                    string twoCharAmount = $"{amount[2]}{amount[3]}";
+                    ConvertWithOneNumber(amount, singleNumberDictionary, doubleNumberDictionary, numberLength, localTrueAndAccount);
+                    ConvertWithDecimals(twoCharAmount, singleNumberDictionary, doubleNumberDictionary);
+                    break;
+
+                case 5:
+                    twoCharAmount = $"{amount[3]}{amount[4]}";
+
+                    ConvertWithTwoNumbers(amount, singleNumberDictionary, doubleNumberDictionary, numberLength, localTrueAndAccount);
+                    ConvertWithDecimals(twoCharAmount, singleNumberDictionary, doubleNumberDictionary);
+                    break;
+
+                case 6:
+                    twoCharAmount = $"{amount[4]}{amount[5]}";
+
+                    ConvertWithThreeNumbers(amount, singleNumberDictionary, doubleNumberDictionary, numberLength, localTrueAndAccount);
+                    ConvertWithDecimals(twoCharAmount, singleNumberDictionary, doubleNumberDictionary);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void ConvertWithThreeNumbers(string amount, Dictionary<char, string> singleNumberDictionary, Dictionary<string, string> doubleNumberDictionary, int numberLength, string localTrueAndAccount)
+        {
+            string doubleAmount = $"{amount[1]}{amount[2]}";
+
+            if (amount[0] == '0')
+            {
+                if (amount[1] == '0' && amount[2] == '0')
+                {
+                    NumbersAsWords += "";
+                    return;
+                }
+                else if (singleNumberDictionary.ContainsKey(amount[2]) && amount[1] == '0')
+                {
+                    NumbersAsWords += $" {singleNumberDictionary[amount[2]]}";
+                }
+                else if (doubleNumberDictionary.ContainsKey(amount[1].ToString()) && amount[2] != '0')
+                {
+                    NumbersAsWords += $" {doubleNumberDictionary[amount[1].ToString()]} {singleNumberDictionary[amount[2]]}";
+                }
+                else if (singleNumberDictionary.ContainsKey(amount[2]) && amount[1] == '0')
+                {
+                    NumbersAsWords += $" {singleNumberDictionary[amount[2]]}";
+                }
+                else if (doubleNumberDictionary.ContainsKey(amount[1].ToString()))
+                {
+                    NumbersAsWords += $" {doubleNumberDictionary[amount[1].ToString()]}";
+                }
+                else if (doubleNumberDictionary.ContainsKey(doubleAmount))
+                {
+                    NumbersAsWords += $" {doubleNumberDictionary[doubleAmount]}";
+                }
+            }
+            else
+            {
+                NumbersAsWords += $" {singleNumberDictionary[amount[0]]} hundred";
+
+                if (amount[1] == '0' && amount[2] == '0')
+                {
+                }
+                else if (doubleNumberDictionary.ContainsKey(amount[1].ToString()) && amount[2] != '0')
+                {
+                    NumbersAsWords += $" and {doubleNumberDictionary[amount[1].ToString()]} {singleNumberDictionary[amount[2]]}";
+                }
+                else if (singleNumberDictionary.ContainsKey(amount[2]) && amount[1] == '0')
+                {
+                    NumbersAsWords += $" and {singleNumberDictionary[amount[2]]}";
+                }
+                else if (doubleNumberDictionary.ContainsKey(amount[1].ToString()))
+                {
+                    NumbersAsWords += $" and {doubleNumberDictionary[amount[1].ToString()]}";
+                }
+                else if (doubleNumberDictionary.ContainsKey(doubleAmount))
+                {
+                    NumbersAsWords += $" and {doubleNumberDictionary[doubleAmount]}";
+                }
+            }
+
+            ConvertLargeNumbers(numberLength, localTrueAndAccount);
+        }
+
+        public static void ConvertWithTwoNumbers(string amount, Dictionary<char, string> singleNumberDictionary, Dictionary<string, string> doubleNumberDictionary, int numberLength, string localTrueAndAccount)
+        {
+            string doubleAmount = $"{amount[0]}{amount[1]}";
+
+            if (doubleNumberDictionary.ContainsKey(amount[0].ToString()) && amount[1] != '0')
+            {
+                NumbersAsWords += $" {doubleNumberDictionary[amount[0].ToString()]} {singleNumberDictionary[amount[1]]}";
+            }
+            else if (doubleNumberDictionary.ContainsKey(amount[0].ToString()))
+            {
+                NumbersAsWords += $" {doubleNumberDictionary[amount[0].ToString()]}";
+            }
+            else if (doubleNumberDictionary.ContainsKey(doubleAmount))
+            {
+                NumbersAsWords += $" {doubleNumberDictionary[doubleAmount]}";
+            }
+
+            ConvertLargeNumbers(numberLength, localTrueAndAccount);
+        }
+
+        public static void ConvertWithOneNumber(string amount, Dictionary<char, string> singleNumberDictionary, Dictionary<string, string> doubleNumberDictionary, int numberLength, string localTrueAndAccount)
+        {
+            if (amount[0] == '0')
+            {
+                NumbersAsWords += " zero";
+            }
+            else if (singleNumberDictionary.ContainsKey(amount[0]))
+            {
+                NumbersAsWords += $" {singleNumberDictionary[amount[0]]}";
+            }
+
+            if (amount[0] == '1' && numberLength == 4)
+            {
+                OneDollar = true;
+            }
+
+            ConvertLargeNumbers(numberLength, localTrueAndAccount);
+        }
+
+        public static void ConvertWithDecimals(string twoCharAmount, Dictionary<char, string> singleNumberDictionary, Dictionary<string, string> doubleNumberDictionary)
+        {
+
+            string doubleAmount = $"{twoCharAmount[0]}{twoCharAmount[1]}";
+
+            if (!OneDollar && doubleAmount == "00")
+            {
+                NumbersAsWords += " dollars and zero cents";
+            }
+            else if (OneDollar && doubleAmount == "00")
+            {
+                NumbersAsWords += " dollar and zero cents";
+            }
+            else if (OneDollar && doubleAmount != "01")
+            {
+                if (doubleNumberDictionary.ContainsKey(twoCharAmount[0].ToString()) && twoCharAmount[1] != '0')
+                {
+                    NumbersAsWords += $" dollar and {doubleNumberDictionary[twoCharAmount[0].ToString()]} {singleNumberDictionary[twoCharAmount[1]]} cents";
+                }
+                else if (singleNumberDictionary.ContainsKey(twoCharAmount[1]) && twoCharAmount[0] == '0')
+                {
+                    NumbersAsWords += $" dollar and {singleNumberDictionary[twoCharAmount[1]]} cents";
+                }
+                else if (doubleNumberDictionary.ContainsKey(twoCharAmount[0].ToString()))
+                {
+                    NumbersAsWords += $" dollar and {doubleNumberDictionary[twoCharAmount[0].ToString()]} cents";
+                }
+                else if (doubleNumberDictionary.ContainsKey(doubleAmount))
+                {
+                    NumbersAsWords += $" dollar and {doubleNumberDictionary[doubleAmount]} cents";
+                }
+            }
+            else if (OneDollar && doubleAmount == "01")
+            {
+                NumbersAsWords += $" dollar and one cent";
+            }
+            else if (!OneDollar && doubleAmount == "01")
+            {
+                NumbersAsWords += $" dollars and one cent";
+            }
+            else
+            {
+                if (doubleNumberDictionary.ContainsKey(twoCharAmount[0].ToString()) && twoCharAmount[1] != '0')
+                {
+                    NumbersAsWords += $" dollars and {doubleNumberDictionary[twoCharAmount[0].ToString()]} {singleNumberDictionary[twoCharAmount[1]]} cents";
+                }
+                else if (singleNumberDictionary.ContainsKey(twoCharAmount[1]) && twoCharAmount[0] == '0')
+                {
+                    NumbersAsWords += $" dollars and {singleNumberDictionary[twoCharAmount[1]]} cents";
+                }
+                else if (doubleNumberDictionary.ContainsKey(twoCharAmount[0].ToString()))
+                {
+                    NumbersAsWords += $" dollars and {doubleNumberDictionary[twoCharAmount[0].ToString()]} cents";
+                }
+                else if (doubleNumberDictionary.ContainsKey(doubleAmount))
+                {
+                    NumbersAsWords += $" dollars and {doubleNumberDictionary[doubleAmount]} cents";
+                }
+            }
+        }
+
+
+        public static void ConvertLargeNumbers(int numberLength, string localTrueAndAccount)
+        {
+            string removedCharLocalTrueAndAccount = localTrueAndAccount.Substring(1);
+
+            if (decimal.Parse(removedCharLocalTrueAndAccount) == 0.00m)
+            {
+                if ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 0)
+                {
+                    NumbersAsWords += " billion";
+                }
+                else if (((numberLength == 12 || numberLength == 11 || numberLength == 10) && LineNumber == 0) || ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 1))
+                {
+                    NumbersAsWords += " million";
+                }
+                else if (((numberLength == 9 || numberLength == 8 || numberLength == 7) && LineNumber == 0) || ((numberLength == 12 || numberLength == 11 || numberLength == 10) && LineNumber == 1) || ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 2))
+                {
+                    NumbersAsWords += " thousand";
+                }
+            }
+            else if (decimal.Parse(removedCharLocalTrueAndAccount) < 99.99m)
+            {
+                if ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 0)
+                {
+                    NumbersAsWords += " billion and";
+                }
+                else if (((numberLength == 12 || numberLength == 11 || numberLength == 10) && LineNumber == 0) || ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 1))
+                {
+                    NumbersAsWords += " million and";
+                }
+                else if (((numberLength == 9 || numberLength == 8 || numberLength == 7) && LineNumber == 0) || ((numberLength == 12 || numberLength == 11 || numberLength == 10) && LineNumber == 1) || ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 2))
+                {
+                    NumbersAsWords += " thousand and";
+                }
+            }
+            else if ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 0)
+            {
+                NumbersAsWords += " billion,";
+            }
+            else if (((numberLength == 12 || numberLength == 11 || numberLength == 10) && LineNumber == 0) || ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 1))
+            {
+                NumbersAsWords += " million,";
+            }
+            else if (((numberLength == 9 || numberLength == 8 || numberLength == 7) && LineNumber == 0) || ((numberLength == 12 || numberLength == 11 || numberLength == 10) && LineNumber == 1) || ((numberLength == 15 || numberLength == 14 || numberLength == 13) && LineNumber == 2))
+            {
+                NumbersAsWords += " thousand,";
+            }
+        }
+    }
+}
